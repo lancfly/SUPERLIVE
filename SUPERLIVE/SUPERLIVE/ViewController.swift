bb6ff8e39528ad0352475d41412609821f88e4f4
gene.dev3 <gene.dev3@le.com>
2016-10-26 11:48:40 +0800

    swift

---
+++ SUPERLIVE/SUPERLIVE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SUPERLIVE
+//
+//  Created by jzkj on 16/10/26.
+//  Copyright © 2016年 XinYu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
